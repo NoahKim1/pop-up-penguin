b516ad8ebc9cbc324fdee37d226347246e0754d0
NoahKim1 <noahkim1234@yahoo.com>
2017-04-01 11:53:34 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function roar(){
 	alert("RAAAAAAWWWWRRRRR!");
 }
+
+document.body.addEventListener('keydown', addStyling);
+document.body.addEventListener('keyup', removeStyling);
+
+
+// function upKey(e) {
+// 	console.log("upKey")
+// 	console.log(e)
+// 	var id = e.key;
+// 	var element = document.getElementById(id);
+	
+// }
